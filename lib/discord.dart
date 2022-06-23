05be1746b9f0d54df9b0d6e42cd0436cabc4d4dd
Willy <willy@coinerella.com>
2022-06-23 17:28:34 +0200

    more messages

--- lib/discord.dart
+++ lib/discord.dart
@@ -25,7 +25,10 @@ void init(String discordToken) async {
 void sendDonationMessage(double amount) {
   List donationMessages = [
     'Someone has just donated $amount Peercoin to the foundation!\nThanks! Wanna donate? https://ppc.lol/fndtn',
-    'Someone has just donated $amount Peercoin to the foundation!\nThanks! Every donations counts! https://ppc.lol/fndtn',
+    'Someone has just donated $amount Peercoin to the foundation!\nThanks! Every donation counts! https://ppc.lol/fndtn',
+    'Wooohoo - another $amount Peercoin donated to the foundation!\nWho is awesome? You are! https://ppc.lol/fndtn',
+    'The Peercoin Foundation relies on your donations to operate.\nThat is why this $amount Peercoin Donation matters so much! https://ppc.lol/fndtn',
+    'Another $amount Peercoin donated to the foundation!\nDonations like this help to further the project immensely. https://ppc.lol/fndtn',
   ];
   sendMessage(donationMessages[Random().nextInt(donationMessages.length)]);
 }
@@ -33,6 +36,8 @@ void sendDonationMessage(double amount) {
 void sendMintMessage(double amount) {
   List mintMessages = [
     'The Peercoin Foundation has just minted $amount Peercoin!\nNice! Wanna know about the Foundation? https://ppc.lol/fndtn',
+    'Minting is fun. So much fun that the Foundation just minted $amount Peercoin!\nWanna know about the Foundation? https://ppc.lol/fndtn',
+    'Minting is easy. So easy that the Foundation just minted $amount Peercoin!\nWanna know about the Foundation? https://ppc.lol/fndtn',
     'The Peercoin Foundation has just minted $amount Peercoin!\nDid you know the Foundation mints through something called Multisig? https://ppc.lol/multisig',
   ];
   sendMessage(mintMessages[Random().nextInt(mintMessages.length)]);
